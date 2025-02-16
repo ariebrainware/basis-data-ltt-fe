42221c44a57f435152702003e1abca2680b23953
Arie Brainware <publicu001@protonmail.com>
2025-02-16 15:57:59 +0700

    feat: remove Example component and its associated button implementation

--- src/app/example/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Button } from "@material-tailwind/react";
- 
-export default function Example() {
-  return <Button color="blue" onClick={() => alert('Button clicked!')}>Button</Button>;
-}
