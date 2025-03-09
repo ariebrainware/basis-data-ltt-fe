3af27e14e49d3597cf4a335c26c677dccdae0bdb
Arie Brainware <publicu001@protonmail.com>
2025-03-09 13:35:55 +0700

    feat: remove commented-out pagination controls from Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -427,27 +427,6 @@ export default function Dashboard() {
             </tbody>
           </table>
         </div>
-        {/* <div className="flex items-center justify-between border-t border-slate-200 py-4">
-          <small className="font-sans text-sm text-current antialiased">
-            Halaman 1 of 10
-          </small>
-          <div className="flex gap-2">
-            <button
-              className="inline-flex select-none items-center justify-center rounded-md border border-slate-200 bg-transparent px-3 py-1.5 text-center align-middle font-sans text-sm font-medium text-slate-800 shadow-sm transition-all duration-300 ease-in hover:bg-slate-200 hover:shadow focus:shadow-none disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none data-[width=full]:w-full data-[shape=pill]:rounded-full"
-              data-shape="default"
-              data-width="default"
-            >
-              Sebelumnya
-            </button>
-            <button
-              className="inline-flex select-none items-center justify-center rounded-md border border-slate-200 bg-transparent px-3 py-1.5 text-center align-middle font-sans text-sm font-medium text-slate-800 shadow-sm transition-all duration-300 ease-in hover:bg-slate-200 hover:shadow focus:shadow-none disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none data-[width=full]:w-full data-[shape=pill]:rounded-full"
-              data-shape="default"
-              data-width="default"
-            >
-              Berikutnya
-            </button>
-          </div>
-        </div> */}
         <Pagination
           currentPage={currentPage}
           setCurrentPage={
