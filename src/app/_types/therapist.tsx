9483392df5d3b84622580a4270fac73a9ee8cfdc
Arie Brainware <publicu001@protonmail.com>
2025-03-21 00:10:27 +0700

    feat: add TherapistType interface for therapist data structure

---
+++ src/app/_types/therapist.tsx
@@ -0,0 +1,12 @@
+export interface TherapistType {
+  full_name: string
+  email: string
+  phone_number: string
+  address: string
+  date_of_birth: string
+  nik: string
+  weight: number
+  height: number
+  role: string
+  is_approved: boolean
+}
